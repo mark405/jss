54275c2be92324c0fe372b771c20b6d640eb8c1e
Mark <mark200320031.tun@gmail.com>
2020-11-19 01:26:27 +0200

    mythirdtask

--- js/firsthomework.js
+++ js/firsthomework.js
@@ -1,15 +1,28 @@
+'use strict';
 // 1
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '0');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '0');
 
 console.log(numberOfFilms);
 
 // 2
 
-const obj = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+// 3
+
+const last = prompt('Один из последних фильмов', ''),
+      ten = prompt('На сколько вы его оцените', ''),
+      last2 = prompt('Один из последних фильмов', ''),
+      ten2 = prompt('На сколько вы его оцените', '');
+
+personalMovieDB.movies[last] = ten;
+personalMovieDB.movies[last2] = ten2;
+
+console.log(personalMovieDB);
