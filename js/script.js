8ff4e8667a78d23fc872daad24d0c4f13758ab4b
Mark <mark200320031.tun@gmail.com>
2020-11-22 00:42:51 +0200

    firstsecondhomework

--- js/script.js
+++ js/script.js
@@ -81,3 +81,120 @@ const ischecked = true,
       isclosed = false;
 
 console.log(ischecked || isclosed);
+
+// Условия
+
+if (4 == 9) {
+    console.log('ok!');
+} else {
+    console.log('error');
+}
+
+
+if (nem < 49) {
+    console.log('error');
+} else if (nem > 100) {
+    console.log('Много');  // стандартная запись
+} else {
+    console.log('ok');
+}
+
+(nem === 50) ? console.log('ok') : console.log('error'); //тернарная запись
+
+const nem = 50;
+
+switch (nem) {  // определяем переменную
+    case 49:  //if (nem == 49)
+        console.log('Неверно');
+        break;
+    case 100: 
+        console.log('Неверно');
+        break;                      //запись swift
+    case 50:
+        console.log('В точку');
+        break;
+    default:  //else
+        console.log('Не в этот раз');
+        break;
+}
+
+// циклы
+
+let pum = 50;
+// while (pum <= 55) {
+//     console.log(pum);
+//     pum++;
+// }
+
+// do {
+//     console.log(pum);
+//     pum++;
+// }
+// while (pum <55);
+
+for (let i = 1; i < 8; i++) {
+    console.log(pum);
+    pum++;
+}
+for (let i=1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+
+    console.log(i);
+}
+
+
+
+// Функции
+
+
+// function declaration
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 20;
+}
+
+showFirstMessage('Hello world');
+ 
+
+let dum = 20;
+
+function showSecondMessage(text) {
+    console.log(text);
+    dum = 30;
+}
+
+showSecondMessage('hello');
+console.log(dum);
+
+
+function calc(a, b) {
+    return (a + b);  //после ruturn код внутри функции становиться нвыполнимым
+}
+
+console.log(calc(5, 6));
+console.log(calc(2, 7));
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const another = ret();
+console.log(another);
+
+//function expression
+
+const logger = function() {
+    console.log('hello');
+};
+
+logger();
+
+//стрелочная функция
+
+const calcu = (a, b) => { return a + b; };
--- js/secondhomework.js
+++ js/secondhomework.js
@@ -1,7 +1,6 @@
 'use strict';
-// 1
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '0');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
 
 console.log(numberOfFilms);
 
@@ -18,40 +17,30 @@ const personalMovieDB = {
 
 if (personalMovieDB.count <= 10) {
     alert('Просмотрено слишком мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
 } else if (personalMovieDB.count >= 30) {
-    alert("Вы киноман");
-} else if (10 < personalMovieDB.count < 30) {
-    alert("Вы классический зритель");
+    alert('Вы киноман');
 } else {
     alert('Произошла ошибка');
 }
-// 3
 
 
-
-for (let i = 1; i < 3; i++) {
-
+let i = 0;
+while (i < 2) {
     const last = prompt('Один из последних фильмов', ''),
-          ten = prompt('На сколько вы его оцените', '');
+          ten = +prompt('На сколько вы его оцените', '');
+    i++;
 
     if (last === null || ten === null || last.length === 0 || ten.length === 0 || last.lenght > 50 || ten.length > 50) {
         console.log('error');
         i--;
     } else {
-        personalMovieDB.movies[last] = ten;
         console.log('ok');
+        personalMovieDB.movies[last] = ten;
     }
+
 }
 
 
-
-
 console.log(personalMovieDB);
-
-
-
-
-// 1
-
-
-
